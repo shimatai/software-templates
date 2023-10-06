86e2f64d3b4a80fa16fe9747627b5388d1fe3b26
Wagner Shimatai de Medio <wagner.medio.capitani@elo.com.br>
2023-10-06 14:56:35 -0300

    Fix test

--- scaffolder-templates/springboot-grpc-template/skeleton/${{values.artifact_id}}.sh
+++ scaffolder-templates/springboot-grpc-template/skeleton/${{values.artifact_id}}.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/bin/java $JVM_DEFAULT_ARGS $JVM_ARGS -jar /usr/share/${{values.artifact_id}}/${{values.artifact_id}}.jar "$@"
+exec /usr/bin/java $JVM_DEFAULT_ARGS $JVM_ARGS -jar /usr/share/${{values.component_id}}/${{values.component_id}}.jar "$@"
